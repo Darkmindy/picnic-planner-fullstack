c76173af9b40706ead96b32f87760665b2e758c1
Fabio Vallacqua <fabio.vallacqua@gmail.com>
2024-06-14 19:27:00 +0200

    feat: adminSignUp and create new route

--- Back_End/src/routes/user.route.ts
+++ Back_End/src/routes/user.route.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import { logIn } from "../controllers/login.controller";
 import { logOut } from "../controllers/logout.controller";
-import { signUp } from "../controllers/signup.controller";
+import { adminSignUp, signUp } from "../controllers/signup.controller";
 import { authMiddleware } from "../middleware/authorization.middleware";
 
 export const router = Router();
 
 router.post("/signup", signUp);
+router.post("/admin-signup", adminSignUp);
 router.post("/login", logIn);
 router.get("/logout", authMiddleware, logOut);

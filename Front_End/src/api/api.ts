a6cf15e32fb57f3768bf22bad9670a79bdea498c
Bruno Rodrigues Silva <149292494+GimbeiNa89@users.noreply.github.com>
2024-06-14 15:59:57 +0200

    feat: first connection front back

--- Front_End/src/api/api.ts
+++ Front_End/src/api/api.ts
@@ -1,24 +1,42 @@
 // src/api/api.ts
-import axios from 'axios';
+import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: "http://localhost:3000",
   headers: {
-    'Content-Type': 'application/json',
+    "Content-Type": "application/json",
   },
 });
 
-export const signUp = async (userData: { name: string, email: string, password: string }) => {
-  const response = await apiClient.post('/signup', userData);
+export const signUp = async (userData: {
+  name: string;
+  email: string;
+  password: string;
+}) => {
+  try {
+    const response = await apiClient.post("/signup", userData);
+    console.log("Response data:", response.data);
+    return response.data;
+  } catch (error) {
+    // Gestione degli errori
+    if (axios.isAxiosError(error)) {
+      // Errore di Axios
+      console.error("Axios error:", error.response?.data || error.message);
+      throw new Error(error.response?.data || "Failed to sign up");
+    } else {
+      // Errore generico
+      console.error("General error:", error);
+      throw new Error("Failed to sign up");
+    }
+  }
+};
+
+export const signIn = async (userData: { email: string; password: string }) => {
+  const response = await apiClient.post("/login", userData);
   return response.data;
 };
 
-export const signIn = async (userData: { email: string, password: string }) => {
-  const response = await apiClient.post('/login', userData);
-  return response.data;
-};
-
-export const logOut = async (userData: { email: string, password: string }) => {
-  const response = await apiClient.post('/logout', userData);
+export const logOut = async (userData: { email: string; password: string }) => {
+  const response = await apiClient.post("/logout", userData);
   return response.data;
 };

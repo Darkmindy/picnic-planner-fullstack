7314d0025e31e943bb618b5c6959a7805670da9b
Andrea-Risiglione <138458914+Andrea-Risiglione@users.noreply.github.com>
2024-06-15 15:32:21 +0200

    feat(): 'add role and checkRole logic'

--- Back_End/src/routes/user.route.ts
+++ Back_End/src/routes/user.route.ts
@@ -3,10 +3,16 @@ import { logIn } from "../controllers/login.controller";
 import { logOut } from "../controllers/logout.controller";
 import { adminSignUp, signUp } from "../controllers/signup.controller";
 import { authMiddleware } from "../middleware/authorization.middleware";
+import { checkRoleMiddleware } from "../middleware/checkRole.middleware";
 
 export const router = Router();
 
 router.post("/signup", signUp);
 router.post("/admin-signup", adminSignUp);
-router.post("/login", logIn);
-router.get("/logout", authMiddleware, logOut);
+router.post("/login", checkRoleMiddleware(["user", "admin"]), logIn);
+router.get(
+	"/logout",
+	authMiddleware,
+	checkRoleMiddleware(["user", "admin"]),
+	logOut
+);

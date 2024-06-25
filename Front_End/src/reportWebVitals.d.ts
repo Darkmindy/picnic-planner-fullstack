5c1865a49ab9ccf61af21ce5c88d3354b9e6a87b
Stefania Deliso <stefania.deliso1@gmail.com>
2024-06-25 13:43:42 +0200

    add reportWebVItals

---
+++ Front_End/src/reportWebVitals.d.ts
@@ -0,0 +1,6 @@
+// src/reportWebVitals.d.ts
+declare module 'reportWebVitals' {
+    const reportWebVitals: (onPerfEntry?: (entry: PerformanceEntry) => void) => void;
+    export default reportWebVitals;
+  }
+  
